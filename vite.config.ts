85b984f4e5069681c9200cdd340f71db6f7e20c2
Ruzanna Martirosyan <ruzanmart1@gmail.com>
2025-09-07 19:09:58 -0700

    fix: set base path to root for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', // 👈 добавляем, чтобы сайт открывался с корня
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+})
